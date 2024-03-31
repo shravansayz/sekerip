5be3512e8d9f064324a1c08f23e9525fa4438769
Shravankumar <shravankumarsayz@gmail.com>
2024-03-31 14:54:24 +0530

    Update apalah.sh

--- apalah.sh
+++ apalah.sh
@@ -9,14 +9,11 @@ do_cleanremove=no
 # Do repo init for rom that we want to build.
 repo init -u https://github.com/Havoc-OS-Revived/android_manifest -b eleven  --git-lfs --depth=1 --no-repo-verify
 
-# Remove tree before cloning our manifest.
-rm -rf device vendor kernel packages/apps/Settings
-
-git clone -b los-q https://github.com/zaidanprjkt/local_manifest .repo/local_manifests
+git clone -b droid https://github.com/shravansayz/local_manifests .repo/local_manifests
 
 # Do remove here before repo sync.
 if [ "$do_cleanremove" = "yes" ]; then
- rm -rf system out prebuilts external hardware packages
+ rm -rf system out prebuilts external hardware
 fi
 
 if [ "$do_smallremove" = "yes" ]; then
@@ -30,16 +27,14 @@ fi
 
 # Do lunch
 . build/envsetup.sh
-lunch havoc_juice-user
+lunch havoc_RMX1901-user
 
-rm -rf packages/apps/Settings
-git clone --depth=1 https://github.com/newestzdn/settings-havoc packages/apps/Settings
 
 # Define build username and hostname things, also kernel
-export BUILD_USERNAME=zaidan
+export BUILD_USERNAME=SHRAVAN
 export BUILD_HOSTNAME=authority
 export SKIP_ABI_CHECKS=true
-export KBUILD_BUILD_USER=zaidan    
+export KBUILD_BUILD_USER=SHRAVAN   
 export KBUILD_BUILD_HOST=authority
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
